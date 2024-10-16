91fd381a797500ac1843af68576ad6ead3ee925b
Arttua_02 <t1aaar01@students.oamk.fi>
2024-10-16 15:15:41 +0300

    valmis vkoteht8

---
+++ app/src/main/java/com/example/mvvmapi/viewmodel/TodoViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.mvvmapi.viewmodel
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvmapi.model.Todo
+import com.example.mvvmapi.model.TodosApi
+import kotlinx.coroutines.launch
+
+class TodoViewModel: ViewModel() {
+    var todos = mutableStateListOf<Todo>()
+        private set
+
+    init {
+        getTodosList()
+    }
+
+    private fun getTodosList() {
+        viewModelScope.launch {
+            var todosApi: TodosApi? = null
+            try {
+                todosApi = TodosApi!!.getInstance()
+                todos.clear()
+                todos.addAll(todosApi.getTodos())
+            } catch (e: Exception) {
+                Log.d("TODOVIEWMODEL", e.message.toString())
+            }
+        }
+    }
+}
